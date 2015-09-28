b0ee09726d4efafee50bd5291b847f4c5769b1d2
JosephRowell <rowelljoey@yahoo.com>
2015-09-28 13:28:20 -0400

    Starting point/Hello World
    Bootstrap and such. Everything we need to get started.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name transferAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the transferAppApp
+ */
+angular.module('transferAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
